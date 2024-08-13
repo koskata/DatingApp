f596568ad2099b5c55a0d96a3b05ea7291570f4e
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-08-13 15:39:24 +0300

    Created data context for the database

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using System;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+}
