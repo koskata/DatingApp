f79263add8eef021c9e5290670d0dc59cfb4a21b
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-08-13 20:34:30 +0300

    Created user controller

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+using System;
+using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // /api/users
+public class UsersController : ControllerBase
+{
+    private readonly DataContext context;
+
+    public UsersController(DataContext _context)
+    {
+        context = _context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        List<AppUser> users = await context.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{id:int}")] // /api/users/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return user;
+    }
+}
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0855fa5d2256fddd6ce357619ffcc067ed4538cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37af18043aac362cb93ed28f0d4a97c7f870bafa")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
