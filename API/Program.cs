d2d842d8267be6a2efa4c06a67a78295205cfe5d
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-08-15 13:47:34 +0300

    Fixed bug with gitignore file

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,8 @@
 using API.Data;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
