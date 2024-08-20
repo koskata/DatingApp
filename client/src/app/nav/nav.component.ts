884940dd05a25e58ff145884f4c099844b271664
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-08-20 14:37:37 +0300

    Coded more logic for login and logout functions

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AccountService } from '../_services/account.service';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, BsDropdownModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
+  accountService = inject(AccountService);
   model: any = {};
 
   login() {
 
-    console.log(this.model);
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+      },
+      error: error => console.error(error)
+      
+    })
+  }
+
+  logout() {
+    this.accountService.logout();
   }
 }
