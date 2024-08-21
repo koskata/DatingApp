649c445c78de42d1099f2724d18dc3a8d9824736
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-08-21 12:24:09 +0300

    Providing toastr

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -4,12 +4,16 @@ import {provideAnimations} from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: 
   [
     provideRouter(routes),
     provideHttpClient(),
-    provideAnimations()
+    provideAnimations(),
+    provideToastr({
+      positionClass: 'toast-bottom-right'
+    })
   ]
 };
