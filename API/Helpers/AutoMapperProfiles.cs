0151039d47a6e7f7c70c4ae45c9c29975bb014b9
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-08-24 18:06:57 +0300

    Added automapper and profiles

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,19 @@
+using System;
+using API.DTOs;
+using API.Extensions;
+using API.Models;
+using AutoMapper;
+
+namespace API.Helpers;
+
+public class AutoMapperProfiles : Profile
+{
+    public AutoMapperProfiles()
+    {
+        CreateMap<AppUser, MemberDto>()
+            .ForMember(d => d.Age, o => o.MapFrom(s => s.DateOfBirth.CalculateAge()))
+            .ForMember(d => d.PhotoUrl, o =>
+                o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url));
+        CreateMap<Photo, PhotoDto>();
+    }
+}
