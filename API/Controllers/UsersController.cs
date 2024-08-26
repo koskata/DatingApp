161e02f735884aae89d63f5956195b5480b07482
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-08-26 20:35:39 +0300

    Added action for updating user

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Interfaces;
@@ -14,10 +15,12 @@ namespace API.Controllers;
 public class UsersController : BaseApiController
 {
     private readonly IUserRepository userRepository;
+    private IMapper mapper;
 
-    public UsersController(IUserRepository _userRepository)
+    public UsersController(IUserRepository _userRepository, IMapper _mapper)
     {
         userRepository = _userRepository;
+        mapper  = _mapper;
     }
 
     [HttpGet]
@@ -39,5 +42,32 @@ public class UsersController : BaseApiController
         }
 
         return user;
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if (username == null)
+        {
+            return BadRequest("No username found in token");
+        }
+
+        var user = await userRepository.GetUserByUsernameAsync(username);
+
+        if (user == null)
+        {
+            return BadRequest("Could not find user!");
+        }
+
+        mapper.Map(memberUpdateDto, user);
+
+        if (await userRepository.SaveAllAsync())
+        {
+            return NoContent();
+        }
+
+        return BadRequest("Failed to update the user!");
     }
 }
