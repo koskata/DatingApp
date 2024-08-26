35a304352133f0775e2553543b1095920fbf91a3
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-08-26 20:38:40 +0300

    Optimizing loading spinner to minimum

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -12,17 +12,17 @@ import { MemberCardComponent } from "../member-card/member-card.component";
 })
 export class MemberListComponent implements OnInit {
   
-  private memberService = inject(MembersService);
-  members: Member[] = [];
+  memberService = inject(MembersService);
   
   ngOnInit(): void {
-    this.loadMembers();
+    if (this.memberService.members().length === 0) {
+      this.loadMembers();
+    }
+    
   }
 
   loadMembers() {
-    this.memberService.getMembers().subscribe({
-      next: members => this.members = members
-    })
+    this.memberService.getMembers()
   }
 
 }
