eba5164c6c77e5c206c99cdb5e4b2c0b73f8d9d6
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-08-29 14:19:59 +0300

    Created class for cloudinary settings

---
+++ API/Helpers/CloudinarySettings.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace API.Helpers;
+
+public class CloudinarySettings
+{
+    public required string CloudName { get; set; }
+
+    public required string ApiKey { get; set; }
+
+    public required string ApiSecret { get; set; }
+}
