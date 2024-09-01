93fc735686e6c7294c8ac03db72ed8fab6c42463
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-09-01 19:09:59 +0300

    Added more properties

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -9,6 +9,21 @@ public class RegisterDto
     public string Username { get; set; } = string.Empty;
 
     [Required]
+    public string? KnownAs { get; set; }
+
+    [Required]
+    public string? Gender { get; set; }
+
+    [Required]
+    public string? DateOfBirth { get; set; }
+
+    [Required]
+    public string? City { get; set; }
+
+    [Required]
+    public string? Country { get; set; }
+
+    [Required]
     [StringLength(8, MinimumLength = 4)]
     public string Password { get; set; } = string.Empty;
 }
