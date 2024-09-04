998a3db2ed60b56b718f17cf814038253cf3311f
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-09-04 14:22:33 +0300

    Restoring tha caching for members list, their details and remembering the filters for searching

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
@@ -13,26 +13,44 @@ import { UserParams } from '../_models/userParams';
 })
 export class MembersService {
   private http = inject(HttpClient);
+  private accountService = inject(AccountService);
   baseUrl = environment.apiUrl;
-  // members = signal<Member[]>([]);
   paginatedResult = signal<PaginatedResult<Member[]> | null>(null);
+  memberCache = new Map();
+  user = this.accountService.currentUser();
+  userParams = signal<UserParams>(new UserParams(this.user));
 
-  getMembers(userParams: UserParams) {
+  resetUserParams() {
+    this.userParams.set(new UserParams(this.user));
+  }
 
-    let params = this.setPaginationHeaders(userParams.pageNumber, userParams.pageSize);
+  getMembers() {
+    const response = this.memberCache.get(Object.values(this.userParams()).join('-'));
 
-    params = params.append('minAge', userParams.minAge);
-    params = params.append('maxAge', userParams.maxAge);
-    params = params.append('gender', userParams.gender);
-    params = params.append('orderBy', userParams.orderBy)
+    if (response) {
+      return this.setPaginatedResponse(response);
+    }
+
+    console.log(Object.values(this.userParams()).join('-'));
+    let params = this.setPaginationHeaders(this.userParams().pageNumber, this.userParams().pageSize);
+
+    params = params.append('minAge', this.userParams().minAge);
+    params = params.append('maxAge', this.userParams().maxAge);
+    params = params.append('gender', this.userParams().gender);
+    params = params.append('orderBy', this.userParams().orderBy)
 
     return this.http.get<Member[]>(this.baseUrl + 'users', {observe: 'response', params}).subscribe({
       next: response => {
-        this.paginatedResult.set({
-          items: response.body as Member[],
-          pagination: JSON.parse(response.headers.get('Pagination')!)
-        })
+        this.setPaginatedResponse(response);
+        this.memberCache.set(Object.values(this.userParams()).join('-'), response);
       }
+    })
+  }
+
+  private setPaginatedResponse(response: HttpResponse<Member[]>) {
+    this.paginatedResult.set({
+      items: response.body as Member[],
+      pagination: JSON.parse(response.headers.get('Pagination')!)
     })
   }
 
@@ -48,11 +66,11 @@ export class MembersService {
   }
 
   getMember(username: string) {
-
-  // const member = this.members().find(x => x.username === username);
-  // if (member !== undefined) {
-  //   return of(member);
-  // }
+    const member: Member = [...this.memberCache.values()].reduce((arr, elem) => arr.concat(elem.body), []).find((m: Member) => m.username === username);
+  
+    if (member) {
+      return of(member);
+    }
 
     return this.http.get<Member>(this.baseUrl + 'users/' + username)
   }
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -17,9 +17,7 @@ import { ButtonsModule } from 'ngx-bootstrap/buttons';
 })
 export class MemberListComponent implements OnInit {
   
-  private accountService = inject(AccountService);
   memberService = inject(MembersService);
-  userParams = new UserParams(this.accountService.currentUser());
   genderList = [{value: 'male', display: 'Males'}, {value: 'female', display: 'Females'}]
   
   ngOnInit(): void {
@@ -30,17 +28,17 @@ export class MemberListComponent implements OnInit {
   }
 
   loadMembers() {
-    this.memberService.getMembers(this.userParams);
+    this.memberService.getMembers();
   }
 
   resetFilters() {
-    this.userParams = new UserParams(this.accountService.currentUser());
+    this.memberService.resetUserParams();
     this.loadMembers();
   }
 
   pageChanged(event: any) {
-    if (this.userParams.pageNumber !== event.page) {
-      this.userParams.pageNumber = event.page;
+    if (this.memberService.userParams().pageNumber !== event.page) {
+      this.memberService.userParams().pageNumber = event.page;
       this.loadMembers();
     }
   }
