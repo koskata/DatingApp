dfacb0532af76899eacfe5a47f12a97ae00d07be
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-09-04 13:51:36 +0300

    Added another claim for id

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -28,7 +28,8 @@ public class TokenService : ITokenService
 
         var claims = new List<Claim>
         {
-            new Claim(ClaimTypes.NameIdentifier, user.UserName)
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Name, user.UserName)
         };
 
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
