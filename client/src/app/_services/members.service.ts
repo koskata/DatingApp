211258385fc0e0ecca0a75a0b619c3f4d02be635
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-09-04 13:53:28 +0300

    Added filters when getting members

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,10 +1,12 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
 import { AccountService } from './account.service';
 import { of, tap } from 'rxjs';
 import { Photo } from '../_models/photo';
+import { PaginatedResult } from '../_models/pagination';
+import { UserParams } from '../_models/userParams';
 
 @Injectable({
   providedIn: 'root'
@@ -12,20 +14,45 @@ import { Photo } from '../_models/photo';
 export class MembersService {
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
-  members = signal<Member[]>([]);
+  // members = signal<Member[]>([]);
+  paginatedResult = signal<PaginatedResult<Member[]> | null>(null);
 
-  getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users').subscribe({
-      next: members => this.members.set(members)
+  getMembers(userParams: UserParams) {
+
+    let params = this.setPaginationHeaders(userParams.pageNumber, userParams.pageSize);
+
+    params = params.append('minAge', userParams.minAge);
+    params = params.append('maxAge', userParams.maxAge);
+    params = params.append('gender', userParams.gender);
+    params = params.append('orderBy', userParams.orderBy)
+
+    return this.http.get<Member[]>(this.baseUrl + 'users', {observe: 'response', params}).subscribe({
+      next: response => {
+        this.paginatedResult.set({
+          items: response.body as Member[],
+          pagination: JSON.parse(response.headers.get('Pagination')!)
+        })
+      }
     })
+  }
+
+  private setPaginationHeaders(pageNumber: number, pageSize: number) {
+    let params = new HttpParams();
+
+    if (pageNumber && pageSize) {
+        params = params.append('pageNumber', pageNumber);
+        params = params.append('pageSize', pageSize);
+    }
+
+    return params;
   }
 
   getMember(username: string) {
 
-  const member = this.members().find(x => x.username === username);
-  if (member !== undefined) {
-    return of(member);
-  }
+  // const member = this.members().find(x => x.username === username);
+  // if (member !== undefined) {
+  //   return of(member);
+  // }
 
     return this.http.get<Member>(this.baseUrl + 'users/' + username)
   }
@@ -33,37 +60,37 @@ export class MembersService {
   updateMember(member: Member)
   {
     return this.http.put(this.baseUrl + 'users', member).pipe(
-      tap(() => {
-        this.members.update(members => members.map(m => m.username === member.username ? member : m))
-      })
+      // tap(() => {
+      //   this.members.update(members => members.map(m => m.username === member.username ? member : m))
+      // })
     )
   }
 
   setMainPhoto(photo: Photo) {
     return this.http.put(this.baseUrl + 'users/set-main-photo/' + photo.id, {}).pipe(
-      tap(() => {
-        this.members.update(members => members.map(m => {
-          if (m.photos.includes(photo)) {
-            m.photoUrl = photo.url;
-          }
+      // tap(() => {
+      //   this.members.update(members => members.map(m => {
+      //     if (m.photos.includes(photo)) {
+      //       m.photoUrl = photo.url;
+      //     }
 
-          return m;
-        }))
-      })
+      //     return m;
+      //   }))
+      // })
     )
   }
 
   deletePhoto(photo: Photo) {
     return this.http.delete(this.baseUrl + 'users/delete-photo/' + photo.id).pipe(
-      tap(() => {
-        this.members.update(members => members.map(m => {
-          if (m.photos.includes(photo)) {
-            m.photos = m.photos.filter(x => x.id !== photo.id)
-          }
+      // tap(() => {
+      //   this.members.update(members => members.map(m => {
+      //     if (m.photos.includes(photo)) {
+      //       m.photos = m.photos.filter(x => x.id !== photo.id)
+      //     }
 
-          return m
-        }))
-      })
+      //     return m
+      //   }))
+      // })
     );
   }
 }
