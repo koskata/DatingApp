1971f3e4c4a336c846ccbe6616cd76e77feffc58
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-09-04 13:54:27 +0300

    Added pagination and filters to members list

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -2,27 +2,47 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { Member } from '../../_models/member';
 import { MemberCardComponent } from "../member-card/member-card.component";
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { AccountService } from '../../_services/account.service';
+import { UserParams } from '../../_models/userParams';
+import { FormsModule } from '@angular/forms';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [MemberCardComponent],
+  imports: [MemberCardComponent, PaginationModule, FormsModule, ButtonsModule],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
 export class MemberListComponent implements OnInit {
   
+  private accountService = inject(AccountService);
   memberService = inject(MembersService);
+  userParams = new UserParams(this.accountService.currentUser());
+  genderList = [{value: 'male', display: 'Males'}, {value: 'female', display: 'Females'}]
   
   ngOnInit(): void {
-    if (this.memberService.members().length === 0) {
+    if (!this.memberService.paginatedResult()) {
       this.loadMembers();
     }
     
   }
 
   loadMembers() {
-    this.memberService.getMembers()
+    this.memberService.getMembers(this.userParams);
+  }
+
+  resetFilters() {
+    this.userParams = new UserParams(this.accountService.currentUser());
+    this.loadMembers();
+  }
+
+  pageChanged(event: any) {
+    if (this.userParams.pageNumber !== event.page) {
+      this.userParams.pageNumber = event.page;
+      this.loadMembers();
+    }
   }
 
 }
