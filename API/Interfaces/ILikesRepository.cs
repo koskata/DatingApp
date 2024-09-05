6c528b11031135c69a7a33db617ad0e81b38b7bc
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-09-05 15:13:18 +0300

    Created and implemented methods for liking

---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,21 @@
+using System;
+using API.DTOs;
+using API.Helpers;
+using API.Models;
+
+namespace API.Interfaces;
+
+public interface ILikesRepository
+{
+    Task<UserLike?> GetUserLike(int sourceUserId, int targetUserId);
+
+    Task<PagedList<MemberDto>> GetUserLikes(LikesParams likesParams);
+
+    Task<IEnumerable<int>> GetCurrentUserLikeIds(int currentUserId);
+
+    void DeleteLike(UserLike like);
+
+    void AddLike(UserLike like);
+
+    Task<bool> SaveChanges();
+}
