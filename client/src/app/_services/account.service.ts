e2473624e413a8e1efe9f2dc96ae9a75a4d14ff6
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-09-05 15:16:31 +0300

    Getting liked Ids when setting current user

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable, signal } from '@angular/core';
 import { User } from '../_models/user';
 import { map } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { LikesService } from './likes.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ import { environment } from '../../environments/environment';
 export class AccountService {
 
   private http = inject(HttpClient);
-
+  private likeService = inject(LikesService);
   baseUrl = environment.apiUrl;
   currentUser = signal<User | null>(null);
 
@@ -38,6 +39,7 @@ export class AccountService {
   setCurrentUser(user: User) {
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUser.set(user);
+    this.likeService.getLikeIds();
   }
 
   logout() {
