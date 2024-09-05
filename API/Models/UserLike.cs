405a663809fe6ab7e4c2342d54a50daf1797642f
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-09-05 15:11:09 +0300

    Created table

---
+++ API/Models/UserLike.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace API.Models;
+
+public class UserLike
+{
+    public AppUser SourceUser { get; set; } = null!;
+
+    public int SourceUserId { get; set; }
+
+    public AppUser TargetUser { get; set; } = null!;
+
+    public int TargetUserId { get; set; }
+}
