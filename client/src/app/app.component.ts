cd6e15021b96f8faa09f91da1adaaf315b7babb7
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-09-05 15:16:04 +0300

    Changed name of method which is for setting current user

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -26,7 +26,7 @@ export class AppComponent implements OnInit{
       return;
     }
     const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
+    this.accountService.setCurrentUser(user);
   }
 
   
