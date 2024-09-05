bc3029a1cc7c761631be7a18f38a72a245001d16
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-09-05 15:14:05 +0300

    Created class for pagination like params

---
+++ API/Helpers/LikesParams.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace API.Helpers;
+
+public class LikesParams : PaginationParams
+{
+    public int UserId { get; set; }
+
+    public required string Predicate { get; set; } = "liked";
+}
