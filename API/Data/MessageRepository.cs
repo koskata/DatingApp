f6ae775130485752ddf63930539c35e2c5ae7404
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-09-09 12:38:07 +0300

    Created repository and implemented some methods

---
+++ API/Data/MessageRepository.cs
@@ -0,0 +1,55 @@
+using System;
+using API.DTOs;
+using API.Helpers;
+using API.Interfaces;
+using API.Models;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+
+namespace API.Data;
+
+public class MessageRepository(DataContext context, IMapper mapper) : IMessageRepository
+{
+    public void AddMessage(Message message)
+    {
+        context.Messages.Add(message);
+    }
+
+    public void DeleteMessage(Message message)
+    {
+        context.Messages.Remove(message);
+    }
+
+    public async Task<Message?> GetMessage(int id)
+    {
+        return await context.Messages.FindAsync(id);
+    }
+
+    public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
+    {
+        var query = context.Messages
+            .OrderByDescending(x => x.MessageSent)
+            .AsQueryable();
+
+        query = messageParams.Container switch 
+        {
+            "Inbox" => query.Where(x => x.Recipient.UserName == messageParams.Username),
+            "Outbox" => query.Where(x => x.Sender.UserName == messageParams.Username),
+            _ => query.Where(x => x.Recipient.UserName == messageParams.Username && x.DateRead == null)
+        };
+
+        var messages = query.ProjectTo<MessageDto>(mapper.ConfigurationProvider);
+
+        return await PagedList<MessageDto>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
+    }
+
+    public Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> SaveAllAsync()
+    {
+        return await context.SaveChangesAsync() > 0;
+    }
+}
---
+++ API/Interfaces/IMessageRepository.cs
@@ -0,0 +1,21 @@
+using System;
+using API.DTOs;
+using API.Helpers;
+using API.Models;
+
+namespace API.Interfaces;
+
+public interface IMessageRepository
+{
+    void AddMessage(Message message);
+
+    void DeleteMessage(Message message);
+
+    Task<Message?> GetMessage(int id);
+
+    Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams);
+
+    Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername);
+
+    Task<bool> SaveAllAsync();
+}
