2d0f02cf50eaaabc7bf6afc2004ce0f78fe5c89e
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-09-09 12:38:20 +0300

    Created DTOs

---
+++ API/DTOs/CreateMessageDto.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace API.DTOs;
+
+public class CreateMessageDto
+{
+    public required string RecipientUsername { get; set; }
+
+    public required string Content { get; set; }
+}
